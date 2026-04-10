3a85fcb48fe40a1c83b55d32d5341a20e2ba7604
Cristiano Polo <cristianopolo11@gmail.com>
2026-04-10 06:11:09 -0300

    fix: total removal of lovable references

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,7 +12,8 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  // Removemos o componentTagger que causava o erro de Build
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
